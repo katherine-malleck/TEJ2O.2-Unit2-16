daf8ece7c8e13bc5d42933bc4a9a734aeeb87b86
katherine <katherine.malleck@stu.ocsb.ca>
2026-04-16 13:29:29 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: katherine
+ * Created on: Apr 20266666
+ * This program will send a top secret message to my evil neighbor sophia :)
 */
 
-basic.showString('Hello, World!')
+radio.setGroup(185)
+basic.showIcon(IconNames.Happy)
+
+// while loop
+while (true) {
+    const distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+
+    // if less
+    if (distance < 10) {
+        basic.clearScreen()
+        radio.sendString("Too close Sophia!!")
+        basic.showIcon(IconNames.No)
+        basic.pause(1000)
+    
+    } else {
+        basic.clearScreen()
+        basic.showNumber(distance)
+        basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+    }
+}
